2c5a6dcbbef250f79259fe4bd7140472350a2762
leocucho <leonardocucho@gmail.com>
2022-12-15 13:18:52 -0500

    new

--- mapa.js
+++ mapa.js
@@ -9,14 +9,14 @@ let geojsonMarkerOptions = {
 
 let geojson_estilo = {
   radius: 8,
-  fillColor: "#fff",
-  color: "#fff",
+  color: "#198754",
+  fillColor: "#198754",
   weight: 1,
   opacity: 1,
   fillOpacity: 0.8
 };
 
-let map = L.map('map').setView([4.639386,-74.082412],6)
+let map = L.map('map').setView([-7.469349226618606, -75.9108656520952],6)
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
