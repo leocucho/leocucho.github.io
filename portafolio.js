37017460d6a16332f9f6765b96048a49c23a139b
leocucho <leonardocucho@gmail.com>
2023-01-16 16:09:07 -0500

    other

---
+++ portafolio.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("back-to-top").style.display = "block";
+  } else {
+    document.getElementById("back-to-top").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+document.getElementById("back-to-top").addEventListener("click", topFunction);
