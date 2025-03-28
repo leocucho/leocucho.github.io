5f33693302e36fd84773338e50d36df8870e8ed3
leocucho <leonardocucho@gmail.com>
2025-03-28 10:18:22 -0500

    ss

--- portafolio.js
+++ portafolio.js
@@ -1,26 +1,34 @@
+// Mostrar el botón "Volver al inicio" al hacer scroll
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("back-to-top").style.display = "block";
+    document.getElementById("back-to-top").style.display = "block"; // Mostrar el botón
   } else {
-    document.getElementById("back-to-top").style.display = "none";
+    document.getElementById("back-to-top").style.display = "none"; // Ocultar el botón
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+// Función para desplazar al principio de la página cuando se hace clic en el botón
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0; // Para Safari
+  document.documentElement.scrollTop = 0; // Para otros navegadores
 }
+
+// Agregar evento de clic al botón "back-to-top"
 document.getElementById("back-to-top").addEventListener("click", topFunction);
 
-// cambio idioma
-
-
+// Cambio de idioma
 document.getElementById("English").onclick = function() {
-    window.location.href = "index.html";
+    window.location.href = "index.html"; // Página en inglés
 };
+
 document.getElementById("Spanish").onclick = function() {
-    window.location.href = "index-es.html";
+    window.location.href = "index-es.html"; // Página en español
 };
+
+
+aclib.runAutoTag({
+    zoneId: '3ccxfv1zrc',
+});
+
